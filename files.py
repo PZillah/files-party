d4dd6fe8a3cc90396ff3352a0a6e033b0daf5f0f
priscilla <priscillakung@gmail.com>
2023-06-30 11:24:43 -0700

    dictionary practice

--- files.py
+++ files.py
@@ -2,10 +2,23 @@
 # Count the number of letter grades in a file using a dictionary
 
 letters = ["A", "B", "C", "D", "F"]
-counts = {} # initialize a dictionary with {}
+counts = {} # initialize an empty dictionary with {}
 file = "letter-grades.txt" # open this file
 
 #loop thru all lines in the file
 for line in open(file):
-    print(line)
+    letter = line.replace("\n", "") # replace blank line with nothing so there's no line space between each letter grade.
+    # if there are commas, use split()
+    # print(line) # for testing.
+    count = counts.get(letter, 0) # gets the count of letter if it exists, 0 otherwise.
+    counts[letter] = count + 1 # store that count into the dict
 
+# Print out counts
+print("Letter counts: ")
+for letter in letters:
+    print(letter + ":", counts.get(letter, 0))
+
+# If you're just counting everything in a file and not worry about what's not there. Print out dictionary like so:
+print("Grade counts dictionary:")
+for item in counts.keys():
+    print(item + ":", counts[item])
