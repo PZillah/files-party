fa7fffc6a72545b22ff0457296eaa93abb35228b
priscilla <priscillakung@gmail.com>
2023-06-29 20:34:19 -0700

    solved #4 all by myself!

--- read3_files2.py
+++ read3_files2.py
@@ -4,16 +4,17 @@
 
 dataFiles = ["text1.txt", "text2.txt", "text3.txt"]
 outputFile = open("counts.txt", "w")
-total_line_count = 0
+total_line_count = 0 # aggregator variable
 total_word_count = 0 # aggregator variable
 
 for file in dataFiles:                          # for each file in the list of files,
-    line_count = sum(1 for line in open(file))  # count the number of lines in each file
+    line_count = len(open(file).readlines())    # OR line_count = sum(1 for line in open(file)) # count the number of lines in each file
+    total_line_count += line_count              # calculate total of lines for each file
     word_count = 0
-    for line in open(file):
-        lines = line.split()                        # split the lines into list of words
-        word_count += len(lines)                    # count the words
-        total_word_count += len(lines)
+    for line in open(file):                     # for every line in each file,
+        lines = line.split()                    # split the lines into list of words
+        word_count += len(lines)                # count the words in each line
+        total_word_count += len(lines)          # calculate running total of words in each line
     print(file, ":", line_count, "lines,", word_count, "words", file=outputFile)
 
 print("TOTAL: ", total_line_count, "lines,", total_word_count, "words", file=outputFile)
