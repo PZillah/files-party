9fa8fcd3189070a61f31dd9d77176e7707bca802
priscilla <priscillakung@gmail.com>
2023-06-30 11:39:07 -0700

    started data project practice

---
+++ practice.py
@@ -0,0 +1,8 @@
+# Data project practice
+# Write a program that reads in the sample_grades file and outputs the mean, median, and standard deviation 
+# for the fall & spring semesters. Make sure to write at least two functions in your final solution. 
+# The more generalizable you make your code, the more you may be able to reuse it for your own project later.
+
+# 1st task: read in the data file and print it out to confirm it's working
+# import to take advantage of built-in python functions for mean, median, standard deviation.
+
